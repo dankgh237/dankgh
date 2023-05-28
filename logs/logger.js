5f633c2a3789e6379722d8dbf7164b767b60522e
dankgh237 <dennykvng@gmail.com>
2023-05-28 16:12:52 +0000

    configure node environment

--- logs/logger.js
+++ logs/logger.js
@@ -1,8 +1,14 @@
 const path = require("path");
 const fs = require("fs");
-const logs = path.resolve(__dirname, "../../../tmp/app.log");
-const {log} = require("./log");
+const dotenv = require("dotenv");
+const resolve =url=>path.resolve(__dirname,url);
+dotenv.config({ path: resolve( "../.env") });
 
+
+
+const prod_env = process.env.NODE_ENV=== "production";
+const logs = prod_env ? resolve("../../../tmp/app.log"):resolve("./app.log");
+const {log} = require("./log");
 console.log(logs);
 
 const logger = (req,res,next)=> (fs.writeFile(logs,log(req),
